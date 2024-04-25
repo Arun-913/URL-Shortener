3e1438fa41042b117b35f5c66b2a5970e6ad4567
Arun-913 <arunchau986@gmail.com>
2024-04-25 18:09:17 +0530

    updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,24 +3,18 @@ const app = express();
 import cors from 'cors';
 import {handleGenerateNewShortURL, handleRedirect, handleUserUrl} from './controllers/url';
 import { handleUserSignUp, handleUserSignIn } from './controllers/user';
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+import { Request, Response } from 'express'; 
 
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req: Request, res: Response)=> {
+    res.send('Server is running');
+});
 app.post('/post-url', handleGenerateNewShortURL);
 app.get('/:shortId',  handleRedirect);
 app.post('/signup', handleUserSignUp);
 app.post('/signin', handleUserSignIn);
 app.post('/user-url', handleUserUrl);
-
-
-// async function main() {
-//     await prisma.url.deleteMany({});
-//     console.log('deleted successfully');
-// }
-
-// main()
 
 app.listen(8080, () => console.log("Server started at port 8080"));
